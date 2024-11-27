85504c4c1135936e726fe8c47c075aa0d56cce7f
Brett <Brett@WIPDeveloper.com>
2024-11-27 07:29:40 -0600

    use resources folder

--- alui/src/scripts/alui_core/alui_chat_cap.lua
+++ alui/src/scripts/alui_core/alui_chat_cap.lua
@@ -1,8 +1,9 @@
-local EMCO = require("alui.EMCO")
+local EMCO = require("alui.src.resources.demonnic-MDK-2.10.0.emco")
 alui = alui or {}
 alui.chatcon = Adjustable.Container:new({
     name = "alui chat con",
-    x = "75%", y = "25%",
+    x = "75%",
+    y = "25%",
     attached = "right",
     width = "25%",
     height = "75%",
@@ -12,7 +13,8 @@ alui.chat_cap = EMCO:new({
     name = "alui chat cap",
     allTab = true,
     consoles = { "All", "Say", "Chat", "Mentor", "Newbie" },
-    x = 0, y = 0,
+    x = 0,
+    y = 0,
     width = "100%",
     height = "100%",
     scrollbars = true,
