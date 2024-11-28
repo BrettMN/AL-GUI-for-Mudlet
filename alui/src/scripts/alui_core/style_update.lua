f6dd10e5dfeb9b569bbe4e2bd1fc8c204d2b7e40
Brett <Brett@WIPDeveloper.com>
2024-11-28 00:07:03 -0600

    style

--- alui/src/scripts/alui_core/style_update.lua
+++ alui/src/scripts/alui_core/style_update.lua
@@ -22,12 +22,12 @@ function style_update(event)
             alui.style[f] = v
         end
     end
-    GUI.Style_Gauge_Aim_Control:setValue(alui.style.aim, alui.style.control)
-    GUI.Style_Gauge_Offensive_Dodge:setValue(alui.style.offensive, alui.style.dodge)
-    GUI.Style_Gauge_Daring_Parry:setValue(alui.style.daring, alui.style.parry)
-    GUI.Style_Gauge_Power_Speed:setValue(alui.style.power, alui.style.speed)
-    GUI.Style_Gauge_Attack_Defense:setValue(alui.style.attack, alui.style.defense)
-    
+    GUI.Style_Gauge_Aim_Control:setValue(alui.style.control, 100)
+    GUI.Style_Gauge_Offensive_Dodge:setValue(alui.style.dodge, 100)
+    GUI.Style_Gauge_Daring_Parry:setValue(alui.style.parry, 100)
+    GUI.Style_Gauge_Power_Speed:setValue(alui.style.speed, 100)
+    GUI.Style_Gauge_Attack_Defense:setValue(alui.style.defense, 100)
+
 
 
     -- alui.stylemini:clear()
